2f062e35a064c9a0d643b9712801ce6824ca113c
Erick Marques <erick.marques@logone.com.br>
2024-01-04 19:17:31 -0300

    Primeiro commit

---
+++ src/main/java/com/marqueserick/security/heimdall/HeimdallSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.marqueserick.security.heimdall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeimdallSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeimdallSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marqueserick/security/heimdall/HeimdallSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marqueserick.security.heimdall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeimdallSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
